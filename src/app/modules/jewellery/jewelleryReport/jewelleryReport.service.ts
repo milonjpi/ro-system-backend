63fdc076a33406010910ebf0250b58981904906d
Md. Milon Hosen <milon.jjj@gmail.com>
2025-05-31 11:49:23 +0600

    jewellery report correction

--- src/app/modules/jewellery/jewelleryReport/jewelleryReport.service.ts
+++ src/app/modules/jewellery/jewelleryReport/jewelleryReport.service.ts
@@ -86,33 +86,35 @@ const getTypeWiseSummary = async (): Promise<ITypeWiseSummary[]> => {
 
   const groupedResult = groupBy(preMappedResult, el => el.category);
 
-  const mappedResult = Object.entries(groupedResult).map(el => {
-    const groupType = groupBy(el[1], bl => bl.jewelleryTypeId);
+  const mappedResult = Object.entries(groupedResult)
+    .map(el => {
+      const groupType = groupBy(el[1], bl => bl.jewelleryTypeId);
 
-    return {
-      category: el[0],
-      weight: totalSum(el[1], 'weight'),
-      types: Object.entries(groupType)
-        .map(bl => {
-          const findType = allTypes?.find(cl => cl.id === bl[0]);
+      return {
+        category: el[0],
+        weight: totalSum(el[1], 'weight'),
+        types: Object.entries(groupType)
+          .map(bl => {
+            const findType = allTypes?.find(cl => cl.id === bl[0]);
 
-          return {
-            type: findType?.label || '',
-            weight: totalSum(bl[1], 'weight'),
-            carats: bl[1]
-              ?.map(cl => {
-                const findCarat = allCarats?.find(dl => dl.id === cl.caratId);
-                return {
-                  carat: findCarat?.label || '',
-                  weight: cl._sum?.weight || 0,
-                };
-              })
-              .sort((a, b) => b.carat.localeCompare(a.carat)), // carats sorted by carat label
-          };
-        })
-        .sort((a, b) => a.type.localeCompare(b.type)), // types sorted by type label
-    };
-  });
+            return {
+              type: findType?.label || '',
+              weight: totalSum(bl[1], 'weight'),
+              carats: bl[1]
+                ?.map(cl => {
+                  const findCarat = allCarats?.find(dl => dl.id === cl.caratId);
+                  return {
+                    carat: findCarat?.label || '',
+                    weight: cl._sum?.weight || 0,
+                  };
+                })
+                .sort((a, b) => b.carat.localeCompare(a.carat)), // carats sorted by carat label
+            };
+          })
+          .sort((a, b) => a.type.localeCompare(b.type)), // types sorted by type label
+      };
+    })
+    .sort((a, b) => a.category.localeCompare(b.category));
 
   return mappedResult;
 };
@@ -142,19 +144,27 @@ const getZakat = async (filters: {
 
   const groupedResult = groupBy(result, el => el.category);
 
-  const mappedResult = Object.entries(groupedResult).map(el => ({
-    category: el[0],
-    carats: el[1]?.map(bl => {
-      const findCarat = allCarats?.find(cl => cl.id === bl.caratId);
-      const findRate = allRates?.find(cl => cl.caratId === bl.caratId);
-      return {
-        carat: findCarat?.label,
-        unitPrice: findRate?.price || 0,
-        weight: bl._sum?.weight || 0,
-        price: (findRate?.price || 0) * (bl._sum?.weight || 0),
-      };
-    }),
-  }));
+  const mappedResult = Object.entries(groupedResult)
+    .map(el => ({
+      category: el[0],
+      carats: el[1]
+        ?.map(bl => {
+          const findCarat = allCarats?.find(cl => cl.id === bl.caratId);
+          const findRate = allRates?.find(cl => cl.caratId === bl.caratId);
+          return {
+            carat: findCarat?.label,
+            unitPrice: findRate?.price || 0,
+            weight: bl._sum?.weight || 0,
+            price: (findRate?.price || 0) * (bl._sum?.weight || 0),
+          };
+        })
+        .sort((a, b) => {
+          const itemA = a.carat || '';
+          const itemB = b.carat || '';
+          return itemB.localeCompare(itemA);
+        }),
+    }))
+    .sort((a, b) => a.category.localeCompare(b.category));
 
   return mappedResult;
 };
